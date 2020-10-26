ba4dc53e401c17ee5e1ab61172546a6c18a438e2
FrenchToast <37608784+NewToT@users.noreply.github.com>
2020-10-26 15:37:40 +0800

    Update FibonacciPalindromeImpl.java

--- fibonacci_palindrome/src/main/java/com/test/FibonacciPalindromeImpl.java
+++ fibonacci_palindrome/src/main/java/com/test/FibonacciPalindromeImpl.java
@@ -1,91 +1,76 @@
 package com.test;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
-
+/**
+ * 具体实现
+ *
+ * @author LeoWang
+ */
 public class FibonacciPalindromeImpl implements FibonacciPalindrome {
-    public Pair findFibonacciPalindrome(List<Integer> sequence) {
-
-        if (sequence == null || sequence.isEmpty() || sequence.size() ==1) {
-            return null;
-        }
-        if(sequence.size() ==2){
-            if(sequence.get(0) == sequence.get(1))
-                return new PairImpl(0, 2);
-        }
-
-        Pair palindromePair = findOutPalindrome(sequence);
-
-        if ((int) palindromePair.getSecond() >= 3) {
-            return palindromePair;
-        } else {
-            Pair fibonacci = findOutFibonacci(sequence);
-            return fibonacci;
-        }
-    }
     /**
-     * To find Palindrome sequence
-     * <p>
+     * find pair of fibonacci
      *
-     *
-     * @param sequence
-     * @return A Pair(startIndex, length)
+     * @param sequence A non-empty sequence of unsigned integers.
+     * @return
      */
-    private static PairImpl findOutPalindrome(List<Integer> sequence) {
-        int start = 0, end = 0;
-        int num1, num2, len;
-        for (int i = 0; i < sequence.size(); i++) {
-            num1 = getMaxLength(sequence, i, i);
-            num2 = getMaxLength(sequence, i, i + 1);
-            len = Math.max(num1, num2);
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
+    public Pair findFibonacciPalindrome(List<Integer> sequence) {
+        int start = 0;
+        int length = 0;
+        int tempIndex = 0;
+        int tempLength = 0;
+
+        // seq元素数量<=2
+        if (sequence.size() <= 2) {
+            StringBuilder stringBuilder = new StringBuilder();
+            sequence.forEach(e -> stringBuilder.append(e.toString()));
+            if (!checkPalindrome(stringBuilder.toString())) {
+                return null;
+            }
+
+            length = stringBuilder.length();
+        } else {
+            // seq元素数量 > 2
+            for (int i = 0; i < sequence.size() - 2; i++) {
+                if (sequence.get(i).intValue() == sequence.get(i + 2).intValue() ||
+                        sequence.get(i) + sequence.get(i + 1) == sequence.get(i + 2) ||
+                        sequence.get(i) == sequence.get(i + 1) + sequence.get(i + 2)) {
+                    tempIndex = i;
+                    //拼接后面3个元素，看是否符合要求
+                    StringBuilder stringBuilder = new StringBuilder(sequence.get(i).toString()).append(sequence.get(i + 1).toString()).append(sequence.get(i + 2).toString());
+                    tempLength = stringBuilder.length();
+                    // 是否回文 、 是否最长
+                    if (length < tempLength && checkPalindrome(stringBuilder.toString()) ) {
+                        start = tempIndex;
+                        length = tempLength;
+                    }
+                }
+            }
+
+            // 无符合规则的序列
+            if (length == 0) {
+                return null;
             }
         }
-        //return string.substring(start, end + 1);
-        return new PairImpl(start , end - start + 1);
-    }
-    /**
-     * To find Fibonacci sequence in Palindrome seq
-     * <p>
-     *
-     * @param sequence
-     * @return A Pair(startIndex, length)
-     */
-    private static Pair findOutFibonacci(List<Integer> sequence){
-
-        List<Integer> tempList = new ArrayList<>();
-
-        for(int i=0;i<=sequence.size()-3;i++){
-            tempList = sequence.subList(i,i+3);
-
-            if(tempList.get(0) == tempList.get(2)||
-                    tempList.get(0)+ tempList.get(1) == tempList.get(2)||
-                    tempList.get(0) == tempList.get(1) + tempList.get(2)
-            )
-                return new PairImpl(i+1, 3);
-        }
-        return null;
+        return new PairImpl(start, length);
     }
 
     /**
-     * To get the max length between two results
-     * <p>
+     * 回文验证
      *
-     * @param s
-     * @param left
-     * @param right
-     *
-     * @return A Pair(startIndex, length)
+     * @param str
+     * @return
      */
-    private static int getMaxLength(List<Integer> s, int left, int right) {
-        int leftPrototype = left, rightPrototype= right;
-        while (leftPrototype >= 0 && rightPrototype < s.size() && s.get(leftPrototype) == s.get(rightPrototype)) {
-            leftPrototype--;
-            rightPrototype++;
+    private static Boolean checkPalindrome(String str) {
+        boolean result = false;
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) == str.charAt(str.length() - 1 - i)) {
+                result = true;
+            } else {
+                return result;
+            }
         }
-        return rightPrototype - leftPrototype - 1;
+        return result;
     }
 }
